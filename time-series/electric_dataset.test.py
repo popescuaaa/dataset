7198941cbf51656e1daff2703272cccbf2340a6a
Popescu Andrei <andreigabrielpopescu556@gmail.com>
2021-02-12 15:52:52 +0200

    Added basic version of the test

--- time-series/electric_dataset.test.py
+++ time-series/electric_dataset.test.py
@@ -1,6 +1,6 @@
 from electric_dataset import ElectricProductionDataset, features
 from torch.utils.data import DataLoader
-import torch 
+import torch
 
 if __name__ == '__main__':
 	csv_path = './csv/Electric_Production.csv'
@@ -11,5 +11,4 @@ if __name__ == '__main__':
 	dl = DataLoader(ds, batch_size, shuffle = True, num_workers = 10)
 
 	for idx, data in enumerate(dl):
-		print(data.shape)
 		assert data.shape == torch.Size([batch_size, seq_len]), 'Faild to produce correct samples size'
